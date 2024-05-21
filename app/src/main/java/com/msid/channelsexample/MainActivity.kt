f02b3ca2da841645603f7e90ae7714f562cb4bde
simunde <sidmundece17@gmail.com>
2024-05-21 13:57:17 -0600

    Caught exception in Emitter

--- app/src/main/java/com/msid/channelsexample/MainActivity.kt
+++ app/src/main/java/com/msid/channelsexample/MainActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
@@ -114,6 +115,9 @@ class MainActivity : AppCompatActivity() {
                     throw Exception("Error in Emitter")
                 }
            //}
+        }.catch {
+            Log.d("Siddhesh","Caught in Emitter ")
+
         }
 
     }
